7f3f3278d3bcf9ac533d244809a66db17bd89665
user <gngh0101@gmail.com>
2019-12-04 00:58:07 +0900

    Improve testcase

--- algorithm/src/test/java/com/github/hotire/algorithm/blind2020_01/SolutionTest.java
+++ algorithm/src/test/java/com/github/hotire/algorithm/blind2020_01/SolutionTest.java
@@ -2,21 +2,34 @@ package com.github.hotire.algorithm.blind2020_01;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 
 class SolutionTest {
 
-  @Test
-  void solution() {
+  @ParameterizedTest
+  @MethodSource("provideText")
+  void solution(final String text, final int expected) {
     // Given
-    final String text = "aabbaccc";
     final Solution solution = new Solution();
 
     // When
     final int length = solution.solution(text);
 
     // Then
-    assertThat(length).isEqualTo(7);
+    assertThat(length).isEqualTo(expected);
+  }
+
+  private static Stream<Arguments> provideText() {
+    return Stream.of(
+        Arguments.of("aabbaccc", 7),
+        Arguments.of("ababcdcdababcdcd", 9),
+        Arguments.of("abcabcdede", 8),
+        Arguments.of("abcabcabcabcdededededede", 14),
+        Arguments.of("xababcdcdababcdcd", 17)
+    );
   }
 }
