8dbd48f45c50b3807c3f180c20a227df4efb86b6
user <gngh0101@gmail.com>
2019-12-29 13:20:55 +0900

    Verify result

--- algorithm/src/test/java/com/github/hotire/algorithm/blind2020_04/SolutionTest.java
+++ algorithm/src/test/java/com/github/hotire/algorithm/blind2020_04/SolutionTest.java
@@ -1,23 +1,21 @@
 package com.github.hotire.algorithm.blind2020_04;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author : hotire
- */
+
 class SolutionTest {
 
   @Test
   void solution() {
+    final int[] expected = {3, 2, 4, 1, 0};
     final String[] words = {"frodo", "front", "frost", "frozen","frame" , "kakao"};
     final String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
     final Solution solution = new Solution();
 
     final int[] result = solution.solution(words, queries);
 
-    System.out.println(Arrays.toString(result));
+    assertThat(result).isEqualTo(expected);
   }
 }
