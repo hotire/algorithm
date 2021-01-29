bc5e7cb21a1da46b489550dfa89151aa532c0ab2
hotire <gngh0101@gmail.com>
2021-01-30 02:33:50 +0900

    Add 2021 kakao blind test solution 02

--- src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
+++ src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
@@ -1,21 +1,24 @@
 package com.github.hotire.algorithm.kakao.blind2021.blind2021_02;
 
-import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Map.Entry;
-import java.util.Set;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class SolutionTest {
 
+    private static Stream<Arguments> provideTest() {
+        return Stream.of(
+                Arguments.of(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}, new String[]{"AC", "ACDE", "BCFG", "CDE"}),
+                Arguments.of(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}, new String[]{"ACD", "AD", "ADE", "CD", "XYZ"})
+                Arguments.of(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}, new String[]{"WX", "XY"})
+        );
+    }
+
     @MethodSource
     @ParameterizedTest
-    void solution() {
+    void solution(final String[] orders, final int[] course, final String[] expected) {
 
     }
 }
