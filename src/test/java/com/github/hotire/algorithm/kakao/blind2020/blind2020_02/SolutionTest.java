23aed29e0210f0aead7a88ce73c78542fac4efbe
hotire <gngh0101@gmail.com>
2021-02-01 00:19:10 +0900

    opt

--- src/test/java/com/github/hotire/algorithm/kakao/blind2020/blind2020_02/SolutionTest.java
+++ src/test/java/com/github/hotire/algorithm/kakao/blind2020/blind2020_02/SolutionTest.java
@@ -3,6 +3,7 @@ package com.github.hotire.algorithm.kakao.blind2020.blind2020_02;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
--- src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
+++ src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.stream.Stream;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
