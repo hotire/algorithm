e2e7495d3888b5a6d3f799f24d4f5e1b199ec70e
hotire <gngh0101@gmail.com>
2021-01-31 11:20:17 +0900

    Verfiy 2021 kakao blind test 02

--- src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
+++ src/test/java/com/github/hotire/algorithm/kakao/blind2021/blind2021_02/SolutionTest.java
@@ -1,7 +1,10 @@
 package com.github.hotire.algorithm.kakao.blind2021.blind2021_02;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.stream.Stream;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,14 +14,21 @@ class SolutionTest {
     private static Stream<Arguments> provideTest() {
         return Stream.of(
                 Arguments.of(new String[]{"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"}, new int[]{2, 3, 4}, new String[]{"AC", "ACDE", "BCFG", "CDE"}),
-                Arguments.of(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}, new String[]{"ACD", "AD", "ADE", "CD", "XYZ"})
+                Arguments.of(new String[]{"ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"}, new int[]{2, 3, 5}, new String[]{"ACD", "AD", "ADE", "CD", "XYZ"}),
                 Arguments.of(new String[]{"XYZ", "XWY", "WXA"}, new int[]{2, 3, 4}, new String[]{"WX", "XY"})
         );
     }
 
-    @MethodSource
+    @MethodSource("provideTest")
     @ParameterizedTest
     void solution(final String[] orders, final int[] course, final String[] expected) {
+        // given
+        final Solution solution = new Solution();
 
+        // when
+        final String[] result = solution.solution(orders, course);
+
+        // then
+        assertThat(result).isEqualTo(expected);
     }
 }
