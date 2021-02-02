da91901613e9a8e5af3726f5d0308af6c62d5abc
hotire <gngh0101@gmail.com>
2021-02-02 23:27:27 +0900

    Verify lowerBoundSearchWhenList

--- src/test/java/com/github/hotire/algorithm/base/src/base/AlgorithmUtilsTest.java
+++ src/test/java/com/github/hotire/algorithm/base/src/base/AlgorithmUtilsTest.java
@@ -2,6 +2,8 @@ package com.github.hotire.algorithm.base.src.base;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class AlgorithmUtilsTest {
@@ -12,5 +14,13 @@ class AlgorithmUtilsTest {
         assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(new int[] { 1, 5, 5, 7}, 5)).isEqualTo(1);
         assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(new int[] {1, 3, 5, 7, 7}, 5)).isEqualTo(2);
         assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(new int[] {1, 2, 3, 5, 7, 9, 11, 15}, 5)).isEqualTo(3);
+    }
+
+    @Test
+    void lowerBoundSearchWhenList() {
+        // expected
+        assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(List.of(1, 5, 5, 7), 5)).isEqualTo(1);
+        assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(List.of(1, 3, 5, 7, 7), 5)).isEqualTo(2);
+        assertThat(AlgorithmUtils.getInstance().lowerBoundSearch(List.of(1, 2, 3, 5, 7, 9, 11, 15), 5)).isEqualTo(3);
     }
 }
