df069b89a9bb9aa08b1db445fe2f3e473927526e
hotire <gngh0101@gmail.com>
2021-03-21 16:47:16 +0900

    Add sort

---
+++ src/main/java/com/github/hotire/algorithm/base/src/base/sort/Sort.java
@@ -0,0 +1,10 @@
+package com.github.hotire.algorithm.base.src.base.sort;
+
+import java.util.Arrays;
+
+public class Sort {
+    public static void main(final String ... args) {
+        final int[] arr = new int[]{1, 2, 3};
+        Arrays.sort(arr);
+    }
+}
